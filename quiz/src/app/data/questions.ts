de615631366662390f10f5c8b3d17e25dc943872
Leonardo Zianni <leonardoziannii@gmail.com>
2024-05-21 14:24:22 -0300

    Adição dos arquivos de tipagem

---
+++ quiz/src/app/data/questions.ts
@@ -0,0 +1,14 @@
+import { Questions } from "../types/Questions";
+
+export const questions: Questions[] = [
+    {
+        question: 'Qual ingrediente não pode faltar ?',
+        options: [
+            'Farinha',
+            'Corante',
+            'Água',
+            'Açucar'
+        ],
+        answer: 1
+    }
+]
