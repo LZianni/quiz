01dd60ef34fd172e671e84838cf64d7aaafd8503
Leonardo Zianni <leonardoziannii@gmail.com>
2024-05-21 14:51:40 -0300

    Finalizando a lógica e estilização da primeira pergunta + adição da segunda pergunta

--- quiz/src/app/page.tsx
+++ quiz/src/app/page.tsx
@@ -2,18 +2,24 @@
 "use client"
 
 import { useState } from "react"
+import { QuestionItem } from "./components/QuestionItem"
 import { questions } from "./data/questions"
 
 const Page = () => {
   const [currentQuestion, setCurrentQuestion] = useState(0)
   const title = 'Quiz de Culinária'
+  const handleAnsered = (answer: number) => {}
 
   return(
     <div className="w-full h-screen flex justify-center items-center bg-blue-600">
       <div className="w-full max-w-xl rounded-md bg-white text-black shadow shadow-black">
         <div className="p-5 font-bold text-2xl border-b shadow-gray-300">{title}</div>
         <div className="p-5">
-          ...
+          <QuestionItem 
+          question={questions[currentQuestion]}
+          count={currentQuestion + 1}
+          onAnswer={handleAnsered}
+          />
         </div>
         <div className="p-5 text-center border-t border-gray-300">
           {currentQuestion + 1} de {questions.length} pergunta{questions.length ===1 ? '' : 's'}
